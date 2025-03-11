bbc94d7e58dfcebfc68370cda6bc6750e5c968bf
0Emperor <tounsadisaadsalah@gmail.com>
2025-03-11 12:31:01 +0000

    scroll kinda done

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,11 @@
 import { div } from "../utils/createElement.js"
-
+/*
+<td>
+<h3 id="post-info">id • Post Title</h3>
+<span id="points">999</span> points by <a href="#"><span id="username">User</span></a>
+3 hours ago | <span id="comments">123</span> <a href="#">comments</a>
+</td>
+*/
 export const Post = async (p) => {
     return div("post").Id(p.id).append(
         div("type", p.type),
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { Post } from "./components/Post.js";
 import { fetchApi } from "./utils/fetchApi.js";
+import { throttle } from "./utils/throttle.js";
 const PAGE_SIZE = 10
 const q = (selector) => document.querySelector(selector)
 
@@ -17,23 +18,40 @@ const liveUpdate = () => {
 
     return () => lastId
 }
+const scroll = async (id, main) => {
+    let wH = window.screenY
+    let scly = document.body.clientHeight    
+    if (wH - scly < 10) {
+        console.log(id);
+        
+        let i = 0
+        while (i < PAGE_SIZE) {
+            const post = await fetchApi(`item/${id}`)
+            id--
+            if (post.type !== "comment") {
+                i++
+                main.append(await Post(post))
+            }
+        }
+    }
+}
 
 let posts = []
-
 const main = async () => {
     let id = await fetchApi("maxitem")
     let i = 0
-    let main = q('main')
+    let main = q('.posts')
     while (i < PAGE_SIZE) {
         const post = await fetchApi(`item/${id}`)
-        console.log(post);
-        
         id--
         if (post.type !== "comment") {
             i++
             main.append(await Post(post))
         }
     }
+    window.addEventListener('scroll', () => {        
+        scroll(id, main)
+    })
 }
 
 main()
