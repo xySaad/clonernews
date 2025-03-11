baf759a10945230ce52095e90a4385fc1808d4a0
0Emperor <tounsadisaadsalah@gmail.com>
2025-03-11 13:59:06 +0000

    hello world

--- main.js
+++ main.js
@@ -5,52 +5,57 @@ const PAGE_SIZE = 10;
 const q = (selector) => document.querySelector(selector);
 
 const liveUpdate = () => {
-  let lastId;
-
-  setInterval(async () => {
-    const id = await fetchApi("maxitem");
-    if (lastId && id != lastId) {
-    }
-    lastId = id;
-  }, 5000);
+    let lastId;
+    setInterval(async () => {
+        const id = await fetchApi("maxitem");
+        if (lastId && id != lastId) {
+        }
+        lastId = id;
+    }, 5000);
 };
 
 let fetching = false;
 
 const scroll = async () => {
-  fetching = !fetching;
-  let id = [...document.querySelectorAll(".post")];
-  id = id[id.length - 1].id;
-  let main = q(".posts");
-  let i = 0;
-  while (i < PAGE_SIZE) {
-    const post = await fetchApi(`item/${id}`);
-    id--;
-    if (post.type !== "comment") {
-      i++;
-      main.append(await Post(post));
+    let main = q(".posts");
+    if (fetching) {
+        return
     }
-  }
-  fetching = !fetching;
+    fetching = !fetching;
+    let id = [...document.querySelectorAll(".postContainer")];
+    id = id[id.length - 1].id;
+    console.log(id);
+
+    let i = 0;
+    while (i < PAGE_SIZE) {
+        const post = await fetchApi(`item/${id}`);
+        id--;
+        if (post.type !== "comment") {
+            i++;
+            main.append(await Post(post));
+        }
+    }
+    fetching = !fetching;
 };
 
 let posts = [];
 const main = async () => {
-  let id = await fetchApi("maxitem");
-  let i = 0;
-  let main = q(".posts");
-  while (i < PAGE_SIZE) {
-    const post = await fetchApi(`item/${id}`);
-    id--;
-    if (post && post.type !== "comment" && !post.dead) {
-      i++;
-      main.append(await Post(post));
+    let id = await fetchApi("maxitem");
+    let i = 0;
+    let posts = q(".posts");
+    while (i < PAGE_SIZE) {
+        const post = await fetchApi(`item/${id}`);
+        id--;
+        if (post && post.type !== "comment" && !post.dead) {
+            i++;
+            posts.append(await Post(post));
+        }
     }
-  }
 
-  const throttledScroll = throttle(scroll, 1000);
+    const throttledScroll = throttle(scroll, 1000);
+    console.log(1);
 
-  window.addEventListener("scroll", throttledScroll);
+    q('main').addEventListener("scroll", throttledScroll);
 };
 
 main();
